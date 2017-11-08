7643c9894cbc9dfcbc4ac9dbf5b0e01490ff1b46
flaviendelangle <flaviendelangle@gmail.com>
2017-11-08 11:31:41 +0100

    Hard coded math in Android.pk

--- DepthMap/app/src/main/cpp/Android.mk
+++ DepthMap/app/src/main/cpp/Android.mk
@@ -1,7 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-PROJECT_ROOT:= $(call my-dir)/../../../..
-PROJECT_ROOT_FROM_JNI:= ../../../..
-CVROOT := ${PROJECT_ROOT}/../Modules/OpenCV-android-sdk/sdk/native/jni
+PROJECT_ROOT := C:/Users/Flavien/Documents/Git_Projects/PSTTango
+CVROOT := ${LOCAL_ROOT}/Modules/OpenCV-android-sdk/sdk/native/jni
 
 include $(CLEAR_VARS)
 
@@ -15,19 +14,19 @@ LOCAL_CFLAGS += -std=c++11 -frtti -fexceptions -fopenmp -w
 LOCAL_SRC_FILES := jni_interface.cpp \
                    Depth_map.cpp \
                    YUVDrawable.cpp \
-                   $(PROJECT_ROOT)/../Modules/tango_gl/src/drawable_object.cc \
-                   $(PROJECT_ROOT)/../Modules/tango_gl/src/shaders.cc \
-                   $(PROJECT_ROOT)/../Modules/tango_gl/src/transform.cc \
-                   $(PROJECT_ROOT)/../Modules/tango_gl/src/util.cc \
-                   $(PROJECT_ROOT)/../Modules/tango_gl/src/video_overlay.cc
+                   $(PROJECT_ROOT)/Modules/tango_gl/src/drawable_object.cc \
+                   $(PROJECT_ROOT)/Modules/tango_gl/src/shaders.cc \
+                   $(PROJECT_ROOT)/Modules/tango_gl/src/transform.cc \
+                   $(PROJECT_ROOT)/Modules/tango_gl/src/util.cc \
+                   $(PROJECT_ROOT)/Modules/tango_gl/src/video_overlay.cc
 
-LOCAL_C_INCLUDES := $(PROJECT_ROOT)/../Modules/tango_gl/include \
-                    $(PROJECT_ROOT)/../Modules/third_party/glm \
+LOCAL_C_INCLUDES := $(PROJECT_ROOT)/Modules/tango_gl/include \
+                    $(PROJECT_ROOT)/Modules/third_party/glm \
                     $(CVROOT)/include
 
 LOCAL_LDLIBS    := -llog -lGLESv3 -L$(SYSROOT)/usr/lib -lz
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-add-path, $(PROJECT_ROOT)/../Modules)
+$(call import-add-path, $(PROJECT_ROOT)/Modules)
 $(call import-module,tango_client_api)
 $(call import-module,tango_support)
